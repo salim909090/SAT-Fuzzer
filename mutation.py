4a21e8f56184848b0d9590799343fafa37febb42
mchara01 <marcos@DESKTOP-DA8QRS5.localdomain>
2022-01-27 19:46:50 +0000

    mutation methods added

---
+++ mutation.py
@@ -0,0 +1,53 @@
+
+def fuzzing_data_random(max_length=20, char_start=32, char_range=32):
+    """
+       A string of up to `max_length` characters
+       in the range [`char_start`, `char_start` + `char_range`]
+    """
+    string_length = random.randrange(0, max_length + 1)
+    out = ""
+    for i in range(0, string_length):
+        out += chr(random.randrange(char_start, char_start + char_range))
+    return out
+
+
+'''
+def delete_random_character(s): # CHECK FUNCTIONALITY & ADD TO API
+    """Returns s with a random character deleted"""
+    if s == "":
+        return s
+
+    pos = random.randint(0, len(s) - 1)
+    # print("Deleting", repr(s[pos]), "at", pos)
+    return s[:pos] + s[pos + 1:]
+
+def insert_random_character(s): # CHECK FUNCTIONALITY & ADD TO API
+    """Returns s with a random character inserted"""
+    pos = random.randint(0, len(s))
+    random_character = chr(random.randrange(32, 127))
+    # print("Inserting", repr(random_character), "at", pos)
+    return s[:pos] + random_character + s[pos:]    
+
+def flip_random_character(s):  # CHECK FUNCTIONALITY & ADD TO API
+    """Returns s with a random bit flipped in a random position"""
+    if s == "":
+        return s
+
+    pos = random.randint(0, len(s) - 1)
+    c = s[pos]
+    bit = 1 << random.randint(0, 6)
+    new_c = chr(ord(c) ^ bit)
+    # print("Flipping", bit, "in", repr(c) + ", giving", repr(new_c))
+    return s[:pos] + new_c + s[pos + 1:]
+
+def mutate(s):  # CHECK FUNCTIONALITY & ADD TO API
+    """Return s with a random mutation applied"""
+    mutators = [
+        delete_random_character,
+        insert_random_character,
+        flip_random_character
+    ]
+    mutator = random.choice(mutators)
+    # print(mutator)
+    return mutator(s)
+'''
