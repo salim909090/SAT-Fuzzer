b30005a41f9774f2b6e272f58751e7fe537e26fb
Nadim Rahman <nadimrahman98@gmail.cm>
2022-02-01 15:12:34 +0000

    i hope marcus is okay x

--- corpus_tracker.py
+++ corpus_tracker.py
@@ -1,12 +1,17 @@
 import os
 import subprocess as cmdlineprocess
 
-
+"""Singleton Class to keep track of the coverage and store interesting 
+inputs for further investigation"""
 class Corpus:
     instance = None
 
+    """Each item in the queue stores the input directory and a counter
+    to represent a counter the number of mutations left for each input.
+    Separate queues for both functional and undefined behaviour strategies."""
     interesting_cnfs_queue_ub = []
     interesting_cnfs_queue_fb = []
+
     current_coverage = 0
 
     def __init__(self):
@@ -16,7 +21,7 @@ class Corpus:
         else:
             Corpus.instance = self
 
-    ""
+    """Return the static object"""
     @staticmethod
     def get_instance():
         """ Static access method. """
@@ -26,6 +31,7 @@ class Corpus:
 
         return Corpus.instance
 
+    """Check if the new input improved the coverage of the directory"""
     def compare_current_coverage(self, new_coverage, new_input, mode, tries):
         if new_coverage > self.current_coverage:
             self.current_coverage = new_coverage
@@ -33,11 +39,16 @@ class Corpus:
         else:
             print("not interesting")
 
+    """Add the cnf input to the corpus"""
     def add_cnf(self, new_input, mode, tries):
         print("Found interesting input")
+        print("Adding to queue: "+"("+str(tries)+")"+new_input)
         if mode == "ub":
             self.interesting_cnfs_queue_ub.append([new_input, tries])
+        else:
+            print("not ub")
 
+    """
     def find_coverage(self, path, new_input, mode, tries):
         command = "cd " + path + " \n gcovr"
         subprocess = cmdlineprocess.Popen(command, shell=True, stdout=cmdlineprocess.PIPE)
@@ -54,13 +65,47 @@ class Corpus:
 
         self.compare_current_coverage(final_percentage, new_input, mode, tries)
         return final_percentage
+    """
 
+    """Find the coverage of the given directory"""
+    def find_coverage(self, path, new_input, mode, tries):
+        # Process the shell command and read the output
+        command = "gcov -n " +path+"/*.c"
+        subprocess = cmdlineprocess.Popen(command, shell=True, stdout=cmdlineprocess.PIPE)
+        subprocess_return = subprocess.stdout.read()
+        subprocess_return = subprocess_return.splitlines()
+
+        # Calculate the total coverage of the directory for all the c files
+        accum_covered = 0
+        accum_total = 0
+        for line in subprocess_return:
+            line = line.decode("utf-8")
+            if("Lines executed" in line):
+                info = line.strip("Lines executed:")
+                info_disect = info.split(" ")
+                percentage_disect = info_disect[0]
+                percentage_disect = percentage_disect.strip("%")
+
+                accum_total = accum_total +int(info_disect[2])
+                accum_covered = accum_covered + (float(percentage_disect)/100)*int(info_disect[2])
+            
+        final_percentage = accum_covered/accum_total
+        self.compare_current_coverage(final_percentage, new_input, mode, tries)
+
+        print("Final Coverage: "+str(final_percentage))
+        return final_percentage
+
+    """Initialise inputs in to the queue given a directory path"""
     def initialise_queue(self, input_path, mode, tries):
         for current_input_filename in os.listdir(input_path):
             file_name_full_path = os.path.abspath(os.path.join(input_path, current_input_filename))
+            # Append input to the right queue
             if mode == "ub":
                 self.interesting_cnfs_queue_ub.append([file_name_full_path, tries])
+            elif mode == "fb":
+                self.interesting_cnfs_queue_fb.append([file_name_full_path, tries])
 
+    """Check if a queue is empty for a given mode"""
     def queue_is_empty(self, mode):
         if mode == "ub":
             if len(self.interesting_cnfs_queue_ub) > 0:
@@ -73,14 +118,19 @@ class Corpus:
             else:
                 return True
 
+    """Pop items in a queue if the counter for a specific input hits 0"""
     def pop_queue(self, mode):
         if mode == "ub":
             if self.interesting_cnfs_queue_ub[0][1] > 0:
-                print("test")
+                print("Getting item from queue: "+"("+str(self.interesting_cnfs_queue_ub[0][1])+")"+self.interesting_cnfs_queue_ub[0][0])
                 self.interesting_cnfs_queue_ub[0][1] = self.interesting_cnfs_queue_ub[0][1] - 1
+                print(self.interesting_cnfs_queue_ub)
                 return self.interesting_cnfs_queue_ub[0][0]
             else:
-                return self.interesting_cnfs_queue_ub.pop()[0]
+                print("Getting last item from queue: "+"("+str(self.interesting_cnfs_queue_ub[0][1])+")"+self.interesting_cnfs_queue_ub[0][0])
+                last_item = self.interesting_cnfs_queue_ub.pop(0)
+                print(self.interesting_cnfs_queue_ub)
+                return last_item[0]
         elif mode == "fb":
             if self.interesting_cnfs_queue_fb[0][1] > 0:
                 self.interesting_cnfs_queue_fb[0][1] = self.interesting_cnfs_queue_fb[0][1] - 1
